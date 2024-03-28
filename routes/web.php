c1830c7fe88a0da4c7529e26d12fc54b65b6a561
vbatalov <vbatalov1996@gmail.com>
2024-03-28 09:16:16 +0700

    build: 1.3 delete get route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\Telegram\TelegramApi;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get("register_bot", [TelegramApi::class, "register_bot"]);
-Route::get("getWebhookInfo", [TelegramApi::class, "getWebhookInfo"]);
